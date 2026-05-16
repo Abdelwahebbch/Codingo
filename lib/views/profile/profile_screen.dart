f9faf3bf7501257d3133230bb076a31e99b5473a
mahdibenmakhlouf <ben.makhlouf.mahdi@gmail.com>
2026-05-16 23:59:20 +0100

    delete unused instance

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -33,8 +33,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<DataProvider>(context);
-    final themeManager = Provider.of<ThemeManager>(context);
-    final isDark = themeManager.themeMode == ThemeMode.dark;
+    
     final auth = Provider.of<AuthProvider>(context);
     final user = authService.authProvider.currentUser;
     final String userImage = authService.progress.imageId;
